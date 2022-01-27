354a3dd8cb2afc9856e40b90faff72ccfa2efe84
chikee <royallor@163.com>
2022-01-27 23:08:50 +0800

    :sparkles: vec3 Utility Functions :art: refactor main.cpp use vec3

--- include/vec3.h
+++ include/vec3.h
@@ -55,3 +55,56 @@ public:
 public:
     double e[3];
 };
+
+//vec3 Utility Functions
+inline std::ostream &operator<<(std::ostream &out, const vec3 &v) {
+    return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
+
+inline vec3 operator+(const vec3 &u, const vec3 &v) {
+    return {u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]};
+}
+
+inline vec3 operator-(const vec3 &u, const vec3 &v) {
+    return {u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - u.e[2]};
+}
+
+inline vec3 operator*(const vec3 &u, const vec3 &v) {
+    return {u.e[0] * v.e[0], u[1] * v.e[1], u.e[2] * v.e[2]};
+}
+
+inline vec3 operator*(double t, const vec3 &u) {
+    return {u.e[0] * t, u.e[1] * t, u.e[2] * t};
+}
+
+inline vec3 operator*(const vec3 &u, double t) {
+    return t * u;
+}
+
+inline vec3 operator/(vec3 v, double t) {
+    return (1 / t) * v;
+}
+
+//点乘
+inline double dot(const vec3 &u, const vec3 &v) {
+    return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2];
+}
+
+/**
+ * 两个向量的叉乘
+ * 结果为vec3
+ * 分量为余子式的值
+ * @param u
+ * @param v
+ * @return
+ */
+inline vec3 cross(const vec3 &u, const vec3 &v) {
+    return {u.e[1] * v.e[2] - u.e[2] * v.e[1],
+            u.e[2] * v.e[0] - u.e[0] * v.e[2],
+            u.e[0] * v.e[1] - u.e[1] * v.e[0]};
+}
+
+//单位化
+inline vec3 unit_vector(vec3 v){
+    return v/v.length();
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "vec3.h"
+
 int main() {
     const int image_width = 200;
     const int image_height = 100;
@@ -9,13 +10,10 @@ int main() {
     for (int j = image_height - 1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            auto r = double(i) / image_width;
-            auto g = double(j) / image_height;
-            auto b = 0.2;
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            vec3 color(double(i) / image_width, double(j) / image_height, 0.2);
+            color.write_color(std::cout);
         }
     }
+
+    std::cerr << "\nDone.\n";
 }
