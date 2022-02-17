d1372ec5f27f060d51a10bc11fd9fa536e35c868
Chikee <royallor@163.com>
2022-02-18 00:01:42 +0800

    :art: 改进hit_sphere代码

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,17 @@
 double hit_sphere(const point3 &center, double radius, const ray &r)
 {
     auto oc = r.origin() - center;
-    auto a = dot(r.direction(), r.direction());
-    auto b = 2.0 * dot(oc, r.direction());
-    auto c = dot(oc, oc) - radius * radius;
-    auto discriminant = b * b - 4 * a * c;
+    auto a = r.direction().length_squared();
+    auto half_b = dot(oc, r.direction());
+    auto c = r.direction().length_squared() - radius * radius;
+    auto discriminant = half_b * half_b - a * c;
     if (discriminant < 0)
     {
         return -1.0;
     }
     else
     {
-        return (-b - sqrt(discriminant)) / (2.0 * a); //不管结果正负，取离我们视角近的点即可
+        return (-half_b - sqrt(discriminant)) / a; //不管结果正负，取离我们视角近的点即可
     }
 }
 
